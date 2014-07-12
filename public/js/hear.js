6b034a0252d750f3970ff6a3d8e5bda31ea77fb8
Karan Goel <karan@goel.im>
2014-07-12 00:18:21 -0700

    make hear and speak modules

---
+++ public/js/hear.js
@@ -0,0 +1,50 @@
+var Hear = function(lang, callback) {
+  if (!('webkitSpeechRecognition' in window)) {
+    console.log("speech recognition not supported");
+  } else {
+    var recognition = new webkitSpeechRecognition();
+    var interim_transcript = '';
+    var final_transcript = '';
+
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.onstart = function() {  };
+
+    recognition.onresult = function(event) {
+      var interim_transcript = '';
+
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          final_transcript += event.results[i][0].transcript;
+        } else {
+          interim_transcript += event.results[i][0].transcript;
+        }
+      }
+
+      callback(final_transcript);
+    };
+
+    recognition.onerror = function(event) {
+      if (event.error == 'no-speech') {
+        //
+      }
+      if (event.error == 'audio-capture') {
+        //
+      }
+      if (event.error == 'not-allowed') {
+        //
+      }
+    };
+
+    recognition.onend = function() {
+      if (!final_transcript) {
+        return;
+      }
+      if (window.getSelection) {
+        window.getSelection().removeAllRanges();
+      }
+    };
+    
+  }
+}
