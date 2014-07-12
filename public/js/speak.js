6746eef727a8bd80ec5a3df97c9adfb4885ae9a2
Karan Goel <karan@goel.im>
2014-07-12 02:55:48 -0700

    fix speak.js

--- public/js/speak.js
+++ public/js/speak.js
@@ -1,12 +1,16 @@
-var Speak = function(text, lang) {
+var Speak = (function(text, lang) {
   var msg = new SpeechSynthesisUtterance();
+  msg.text = text;
+
   if (lang == 'zh') {
     msg.voice = 'zh-CN';
   } else if (lang == 'es') {
     msg.voice = 'es-ES';
+  } else if (lang == 'fr') {
+    msg.voice = 'fr-FR';
   } else {
     msg.voice = 'en-US';
   }
-  msg.text = text;
-  window.speechSynthesis.speak(msg);
-}
+
+  speechSynthesis.speak(msg);
+});
