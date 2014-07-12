867ae29cc036a599dd5e26a043594ffe2e89c013
Grant Timmerman <granttimmerman@gmail.com>
2014-07-11 23:47:32 -0700

    Getting dom ready for animations

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,18 @@
 $(function () {
-  console.log('hi');
+  var inPageTransition = false;
+
+  function exitLogin () {
+    $('.page.login .content').hide();
+  }
+  function enterHome () {
+    $('.page.home').css('display', 'block');
+  }
+
+  // Event listeners
+
+  $('.signinButton').click(function() {
+    exitLogin();
+    enterHome();
+    inPageTransition = true;
+  });
 });
