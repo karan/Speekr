bd83b7ba394336b070ae6181e5d56c6058acf1a6
Grant Timmerman <granttimmerman@gmail.com>
2014-07-12 00:05:42 -0700

    Fix animate up

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,37 @@
 $(function () {
+  var ANIMATION_DURATION = 400;
+  var ANIMATION_EASING = 'easeInQuad';
   var inPageTransition = false;
 
+  var $loginPage = $('.page.login');
+  var $homePage = $('.page.home');
+  
+  var $login = $('.page.login .content');
+  var $home = $('.page.home .content');
+
   function exitLogin () {
-    $('.page.login .content').hide();
+    $login.animate({
+      top: '-100%'
+    }, {
+      duration: ANIMATION_DURATION,
+      ease: ANIMATION_EASING,
+      complete: function () {
+        $loginPage.hide();
+        $login.css('top', '0%');
+      }
+    });
   }
   function enterHome () {
-    $('.page.home').css('display', 'block');
+    $homePage.css('display', 'block');
+    $home.css('top', '100%').animate({
+      top: '0%'
+    }, {
+      duration: ANIMATION_DURATION,
+      ease: ANIMATION_EASING,
+      complete: function () {
+        console.log('hi');
+      }
+    });
   }
 
   // Event listeners
