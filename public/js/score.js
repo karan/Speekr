707771f75a0fd7cc88af91d754cbf76170a0a367
Karan Goel <karan@goel.im>
2014-07-12 05:49:02 -0700

    fix scoring algorithm

--- public/js/score.js
+++ public/js/score.js
@@ -1,7 +1,29 @@
 // calculate a score based on actual text and text that user spoke
 
-var Score = function(actualText, userText) {
+var Score = function(actualText, userText, thingType) {
   var l = new Levenshtein(actualText, userText);
   var distance = l.distance;
-  return distance + (actualText.length - userText.length) ^ 2;
+  if (distance == 0) {
+    return Math.pow(actualText.length, 2);
+  }
+
+  var actualThingCount = 0;
+  var userThingCount = 0;
+  switch(thingType) {
+    case "1": 
+      actualThingCount = actualText.length;
+      userThingCount = userText.length;
+      break;
+    case "2":
+      actualThingCount = actualText.split(' ').length;
+      userThingCount = userText.split(' ').length;
+      break;
+    case "3":
+      actualThingCount = actualText.split('.').length;
+      userThingCount = userText.split('.').length;
+      break;
+  }
+  if (distance >= actualThingCount) return 0;
+
+  return distance + Math.pow(actualThingCount - userThingCount, 2);
 }
