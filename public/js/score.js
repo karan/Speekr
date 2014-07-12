ed51088d013f910524e211f93cc96720708ef377
Grant Timmerman <granttimmerman@gmail.com>
2014-07-12 09:57:59 -0700

    Add chinese differ

--- public/js/score.js
+++ public/js/score.js
@@ -1,5 +1,48 @@
 // calculate a score based on actual text and text that user spoke
 
+// var zh_score = function (actualText, userText) {
+//   var i;
+//   var actualCount = {};
+//   var userCount = {};
+//   for (i = 0; i < actualText.length; ++i) {
+//     var c = actualText.charAt(i);
+//     if (typeof actualCount[c] === 'undefined') {
+//       actualCount[c] = 1;
+//     } else {
+//       ++actualCount[c];
+//     }
+//   }
+//   for (i = 0; i < userText.length; ++i) {
+//     var c = userText.charAt(i);
+//     if (typeof userText[c] === 'undefined') {
+//       userCount[c] = 1;
+//     } else {
+//       ++userCount[c];
+//     }
+//   }
+
+//   var maxWords = Math.max(userText.length, actualText.length);
+//   var totalWords = userText.length + actualText.length;
+//   // Go through both to find the word count diff
+//   var missedWords = 0;
+//   var runningCount = 0;
+//   for (i in actualCount) {
+//     var aCount = actualCount[i];
+//     var uCount = userCount[i];
+//     if (!uCount) {
+//       uCount = 0;
+//     }
+//     missedWords += Math.abs(aCount - uCount);
+//     runningCount += aCount + uCount;
+//   }
+
+//   var badWords = missedWords + (totalWords - runningCount)/2;
+//   var goodPercent = 1 - (badWords/totalWords);
+
+//   return goodPercent * 10;
+// };
+
+
 var Score = function(actualText, userText, thingType, lang) {
   var l = new Levenshtein(actualText.toLowerCase(), userText.toLowerCase());
   var distance = l.distance;
